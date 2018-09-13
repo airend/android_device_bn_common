19118c2475dd46d9386d377e4b6bae158bbc83cd
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-09-13 11:02:09 +0300

    init: Explicitly filter recovery arg in BCB script
    Android Pie uses 'shell' as arg for normal reboot, which confuses the
    BCB on shutdown script.
    
    Change-Id: Ieaacb230d611a1a71cf74f5efcc6d506ec5dc124
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- prebuilt/bin/init.pre-rb.sh
+++ prebuilt/bin/init.pre-rb.sh
@@ -4,4 +4,4 @@ csplit() { echo -n $1| cut -d\, -f$2; }
 
 CMD=`csplit $1 1`; ARG=`csplit $1 2`; BCB=/bootdata/BCB
 
-[ "$CMD" = "reboot" ] && [ "$ARG" ] && echo -n $ARG > $BCB
+[ "$CMD" = "reboot" ] && [ "$ARG" = "recovery" ] && echo -n $ARG > $BCB
